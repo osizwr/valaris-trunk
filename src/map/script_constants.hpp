366ba41c559c6166fa6769d0a39d89b9ec105059
Marvin Joseph <202040175@psu.palawan.edu.ph>
2026-07-03 19:26:42 +0800

    missing script_constant.hpp thats what caused the error

--- src/map/script_constants.hpp
+++ src/map/script_constants.hpp
@@ -2005,6 +2005,10 @@
 	export_constant(SC_WIND_CHARM_POWER);
 	export_constant(SC_GROUND_CHARM_POWER);
 
+	// ===== Custom status changes (Hokage) =====
+	export_constant(SC_JINTON);
+	export_constant(SC_SENNIN_MODE);
+
 /// Do not modify code below this, until the end of the API hook, since it will be automatically generated again
 /// @APIHOOK_START(EFST_CONST)
 	export_constant(EFST_BLANK);
