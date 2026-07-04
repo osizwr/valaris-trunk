ab43208846808b9966edb6c3f5bbc18bee32c20f
Marvin Joseph Cajilo <202040175@psu.palawan.edu.ph>
2026-07-04 11:02:23 +0800

    compiler.bat + new client ver

---
+++ compile.bat
@@ -0,0 +1,4 @@
+@echo off
+@REM "G:\Microsoft Visual Studio 2026\MSBuild\Current\Bin\MSBuild.exe" -m
+"C:\Program Files\Microsoft Visual Studio\18\Community\MSBuild\Current\Bin\MSBuild.exe" -m
+pause
